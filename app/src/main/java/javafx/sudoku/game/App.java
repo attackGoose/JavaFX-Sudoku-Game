ef6b53cfd753265b619750e1315c91b486e246de
attackGoose <henrycao0308@gmail.com>
2023-06-28 13:58:11 -0400

    minor changes

--- app/src/main/java/javafx/sudoku/game/App.java
+++ app/src/main/java/javafx/sudoku/game/App.java
@@ -26,29 +26,7 @@ public class App extends Application{
     TextField b21;
     StackPane testPane;
 
-    //box slots/Grid: 
-    /* 
-    int a1, a2, a3, a4, a5, a6, a7, a8, a9, a10;
-    int b1, b2, b3, b4, b5, b6, b7, b8, b9, b10;
-    int c1, c2, c3, c4, c5, c6, c7, c8, c9, c10;
-    int d1, d2, d3, d4, d5, d6, d7, d8, d9, d10;
-    int e1, e2, e3, e4, e5, e6, e7, e8, e9, e10;
-    int f1, f2, f3, f4, f5, f6, f7, f8, f9, f10;
-    int g1, g2, g3, g4, g5, g6, g7, g8, g9, g10;
-    int h1, h2, h3, h4, h5, h6, h7, h8, h9, h10;
-    int i1, i2, i3, i4, i5, i6, i7, i8, i9, i10;*/
-
-    int[][] GameBoard = new int[10][10]; /*{ the starting value of each slot is 0, which wil need to be changed as the puzzle progresses
-        {a1,b1,c1,d1,e1,f1,g1,h1,i1},
-        {a2,b2,c2,d2,e2,f2,g2,h2,i2},
-        {a3,b3,c3,d3,e3,f3,g3,h3,i3},
-        {a4,b4,c4,d4,e4,f4,g4,h4,i4},
-        {a5,b5,c5,d5,e5,f5,g5,h5,i5},
-        {a6,b6,c6,d6,e6,f6,g6,h6,i6},
-        {a7,b7,c7,d7,e7,f7,g7,h7,i7},
-        {a9,b9,c9,d9,e9,f9,g9,h9,i9},
-        {a10,b10,c10,d10,e10,f10,g10,h10,i10}
-        };*/
+    int[][] GameBoard = new int[10][10]; //{ the starting value of each slot is 0, which wil need to be changed as the puzzle progresses
 
 
     Sudoku gamepannel = new Sudoku(GameBoard);
